f80e8247ca77b1c5bf9f997af48dbc741d67a13d
Josselyn Erique <js.eriquer@uea.edu.ec>
2024-02-24 22:08:18 -0500

    Deber semana 11 arreglo bidimensional

---
+++ Arreglo_Multidimensional.py
@@ -0,0 +1,30 @@
+# Crear una matriz bidimensional (3x3)
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+
+# Búsqueda de un valor específico en la matriz
+valor_buscado = 8
+valor_encontrado = False
+fila_encontrada = -1
+columna_encontrada = -1
+
+for fila in range(len(matriz)):
+    for columna in range(len(matriz[fila])):
+        if matriz[fila][columna] == valor_buscado:
+            valor_encontrado = True
+            fila_encontrada = fila
+            columna_encontrada = columna
+            break  # Detener la búsqueda una vez que se encuentra el valor
+    if valor_encontrado:
+        break
+
+# Mostrar el resultado de la búsqueda
+if valor_encontrado:
+    print(f"Se encontró {valor_buscado} en la posición ({fila_encontrada}, {columna_encontrada}) de la matriz.")
+else:
+    print(f"{valor_buscado} no se encontró en la matriz.")
+
+
